84fb6a4f15b744056911a7a6690207f93f13ae7c
amciss <slyamciss@gmail.com>
2022-02-22 09:58:56 -0500

    variables command

--- 02-vars/output.tf
+++ 02-vars/output.tf
@@ -1,3 +1,7 @@
 output "sample" {
   value = var.sample
 }
+
+output "list1" {
+  value = var.list[0]
+}
--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -4,3 +4,26 @@ variable "sample" {
   default = "Hello bilal"
 }
 
+# different data type in terraform. They are all variables
+
+# String Data type : need to be in double quote
+variable "sample1" {
+  default = "Hello World"
+}
+
+## Terraform does not support single quote
+
+# Number Data type : Do not need to be in quote
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data type : Do not need to be in quote
+variable "sample3" {
+  default = true
+}
+
+## List can have values of different types
+variable "list" {
+  default = [ "Hello", 90, true ]
+}
