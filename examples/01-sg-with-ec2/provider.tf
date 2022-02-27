adc32a5d038e5281aca5cb76f8374ae4df18c0c1
slyamciss <slyamciss@gmail.com>
2022-02-27 11:34:41 -0500

    s3 bucket added

--- examples/01-sg-with-ec2/provider.tf
+++ examples/01-sg-with-ec2/provider.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "terraform-amciss"
-    key    = "sample/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+ terraform {
+   backend "s3" {
+     bucket = "terraform-amciss"
+     key    = "sample/terraform.tfstate"
+     region = "us-east-1"
+   }
+ }
