758e0e5bc784356f51ecc2f1a14b5419742ee3a0
slyamciss <slyamciss@gmail.com>
2022-02-27 14:51:26 -0500

    root module

---
+++ examples/02-sg-with-ec2-with-modules/ec2/ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "web" {
+  ami           = "ami-075e5fd9ac82444c1"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [var.SG_ID]
+
+  tags = {
+    Name = "web"
+  }
+}
+variable "SG_ID" {}
